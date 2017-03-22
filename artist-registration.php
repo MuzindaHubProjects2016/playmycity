c4374977fe86b16e16b5bed894988c738b4740e0
Tawanda Musabaika <tmusabaika@gmail.com>
2017-03-23 00:17:41 +0200

    Fixed artist registration

--- artist-registration.php
+++ artist-registration.php
@@ -4,19 +4,16 @@ include('includes/connect.php');
 $flag = true; //TODO: Start with true instead then set false when a field is empty to avoid last item resetting the flag to true
 
 if(isset($_POST['artist_signup'])){ //CHECK if there are any missing fields
-  if(isset($_POST['artist_city'])){
-    // do nothing;
-}else{
-  echo "<br> Please select a city <br>";
-}
+
 foreach($_POST as $key => $value){
-  if(!$value){   // !$value means value is empty
+  if(empty($_POST[$key])){   // !$value means value is empty
     if($key != "artist_signup"){
       $flag = false; //flag should be set here since were are excluding fan_signup. put outside this if, $flag will always be false
       echo $key." is missing <br>";
     }
   }
 }
+
 
 //CHECK if username is already used and redirect to enter a different username
   
@@ -30,19 +27,19 @@ while ($row = mysqli_fetch_assoc($artists)){
   if(!$exists){
     if($flag){
       $artist_stagename = $_POST['artist_stagename'];
+      $artist_password = $_POST['artist_password'];
       $artist_firstname = $_POST['artist_firstname'];
-      $artist_lastname = $_POST['artist_firstname'];
+      $artist_lastname = $_POST['artist_lastname'];
       //$requests = $_POST['requests'];
       //$confirmed_shows = $_POST['confirmed_shows'];
       $main_genre = $_POST['main_genre'];
       $artist_city = $_POST['artist_city'];
       //$artist_profile_pic = $_POST['artist_profile_pic'];
 
-      echo 'Thank you for registering.';
-
-      $sql = "INSERT INTO playmycity_artists VALUES ('','$artist_stagename', '$artist_firstname', '$artist_lastname', '', '', '$main_genre', '$artist_city', '' )";
+      $sql = "INSERT INTO playmycity_artists VALUES ('','$artist_stagename', '$artist_password', '$artist_firstname', '$artist_lastname', '', '', '$main_genre', '$artist_city', '' )";
       //with mysqli you dont have to specify the column names first
       mysqli_query($connect, $sql);
+      header('Location: login.php');
     }else {
       echo "Please fill in the Missing Field";
     }
