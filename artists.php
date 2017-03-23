1c2f1963766ba4075661c107d5064748334b4ed9
Tawanda Musabaika <tmusabaika@gmail.com>
2017-03-23 09:22:05 +0200

    Refactored the Artists Page
    - Added the 'PlayMyCity' button functionality

--- artists.php
+++ artists.php
@@ -9,31 +9,16 @@ $pageTitle = 'PlayMyCity | Artists';
       <ul id="artist-list">
         
         <?php
-        
-        while ($row = mysqli_fetch_assoc($artists)) {
-          
-          //update artist requests from the requests table
-          $artist = $row['artist_id'];
-          $new_requests = "SELECT playmycity_fans.*, playmycity_artists.*
-          FROM playmycity_fans
-          INNER JOIN requests
-          ON playmycity_fans.fan_username = requests.fan_username
-          INNER JOIN playmycity_artists
-          ON requests.artist_id = playmycity_artists.artist_id
-          WHERE requests.artist_id = '$artist'";
-
-          $result = mysqli_query($connect, $new_requests);
-          $rowcount = mysqli_num_rows($result);
-
-          $requests_update = "UPDATE playmycity_artists SET requests='$rowcount'
-          WHERE artist_id = '$artist' ";
-          mysqli_query($connect, $requests_update);
-          
-           //Populate artist list 
-           echo '<a href="fan-artist-view.php?artist='.$row['artist_id'].'"><li id="'.$row['artist_id'].'" class="artist-name"><span id="stage-name">'.$row["artist_stagename"].'</span><span class="artist-image-url">'.$row["artist_profile_pic"].'</span><span class="requests">'.$row["requests"].'</span><span class="confirmed-shows">'.$row["confirmed_shows"].'</span><button class="play-my-city">PlayMyCity</button></li></a>';
-
-          }      
+          while ($row = mysqli_fetch_assoc($artists)) {
         ?>
+<!--       Populate artist list -->
+           <a href="fan-artist-view.php?artist=<?php echo $row['artist_id']; ?>"><li id="<?php echo $row['artist_id']; ?>" class="artist-name"><span id="stage-name"> <?php echo $row['artist_stagename']; ?></span><span class="artist-image-url"><?php echo $row['artist_profile_pic']; ?></span><span class="requests"><?php echo $row['requests']; ?></span><span class="confirmed-shows"><?php echo $row['confirmed_shows']; ?></span></li></a>
+          
+        <form action="request-artist.php?artist=<?php echo $row['artist_id']; ?>" method="post">
+           <button type="submit" name="request_artist" class="play-my-city">PlayMyCity</button>
+          </form>
+
+        <?php  }  ?> <!-- close while statement -->
         
       </ul>
     </div><!-- end artist-list-wrap -->
